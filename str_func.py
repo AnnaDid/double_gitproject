56996ee2f40197c68eb58ba998d325b653a57c28
Anna Didenko <open0907@mail.ru>
2024-06-30 20:06:33 +0300

    changes made

---
+++ str_func.py
@@ -0,0 +1,21 @@
+def change_text():
+    """ПЕРЕВОДИТ В ВЕРХНИЙ РЕГИСТР"""
+    text = input()
+    text = text.upper()
+
+
+
+change_text()
+
+
+
+
+def change_text_2():
+    """Переводит начальные буквы слов в верхний регистр"""
+    text = input()
+    text = text.capitals()
+
+
+change_text_2()
+
+
